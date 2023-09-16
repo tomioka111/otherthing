bbaf5c9b37f810aa07d99edda3b93b3cf65dda01
tomioka111 <128500369+tomioka111@users.noreply.github.com>
2023-09-16 10:15:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['201206178781', 'ɢᴏᴊᴏ', true],
-  ['967780763155', 'BOT', true], 
+  ['966505433177', 'GYU', true],
+  ['994400699215', 'BOT', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['201159250900','201206178781'] 
-global.prems = ['201206178781', '201159250900', '967780763155']
+global.mods = ['966505433177','9665054331771'] 
+global.prems = ['966505433177', '966505433177', '967780763155']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
